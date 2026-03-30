4e40deaf1aeac4c5547876f19180c89b4cab331f
olalekan <officialpurpledragon01@gmail.com>
2026-03-31 00:51:20 +0100

    added cors support

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ require("dotenv").config()
 
 const express = require('express');
 const app = express();
+const cors = require("cors")
+
+app.use(cors())
 app.use(express.json()); // Parse JSON bodies
 
 let todos = [
